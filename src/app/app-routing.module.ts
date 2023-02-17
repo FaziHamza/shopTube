d662a07163cab4e348f3f0c902151d585afb7a40
arfanali278 <arfan7452@gmail.com>
2023-02-17 17:01:09 +0500

    menu work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PagesComponent } from './pages/pages.component';
 import { ZorroComponent } from './zorro/zorro.component';
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: SiteLayoutComponent ,
+  children:[
+    {
+      path: 'pages/:schema',
+      component: PagesComponent
+    },
+  ]},
   { path: 'ZorroComponent', component: ZorroComponent },
-  {
-    path: 'pages/:schema',
-    component: PagesComponent
-  },
+
 ];
 
 @NgModule({
