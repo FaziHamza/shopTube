c472bad1c79ab8368be2bd92a4e4a825bfbe3f0d
arfanali278 <arfan7452@gmail.com>
2023-02-20 21:48:04 +0500

    ng-search

--- src/app/wrappers/formly-field-ng-select.component.ts
+++ src/app/wrappers/formly-field-ng-select.component.ts
@@ -4,43 +4,14 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
   selector: 'app-formly-field-ng-select',
   template: `
-  <nz-select ngModel="lucy">
-      <nz-option nzValue="jack" nzLabel="Jack"></nz-option>
-      <nz-option nzValue="lucy" nzLabel="Lucy"></nz-option>
-      <nz-option nzValue="disabled" nzLabel="Disabled" nzDisabled></nz-option>
+      <nz-select nzShowSearch nzAllowClear nzPlaceHolder="Select a person" [(ngModel)]="selectedValue">
+      <nz-option [nzLabel]="item.label" [nzValue]="item.value" *ngFor="let item of list"></nz-option>
     </nz-select>
-
-  <!-- <ng-select [items]="list"
-      [bindLabel]="labelProp"
-      [bindValue]="valueProp"
-      [multiple]="to.multiple"
-      [placeholder]="to.placeholder"
-      [(ngModel)]="selectedValue"
-      >
-    </ng-select> -->
-
   `,
 })
 export class FormlyFieldNgSelectComponent extends FieldType {
-  // get labelProp(): any {
-  //   return this.to.labelProp || 'label';
-  // }
-  // get labelVal(): any {
-  //   return this.to.label ;
-  // }
-  // get list(): any {
-  //   return this.to.options;
-  // }
-  // get valueProp(): any {
-  //   return this.to.valueProp || 'value';
-  // }
-  // get groupProp(): any {
-  //   return this.to.groupProp || 'group';
-  // }
-  // selectedValue: any;
-
-  // ngOnInit() {
-  //   debugger
-  //   this.selectedValue = this.to.defaultValue;
-  // }
+  selectedValue = null;
+  get list(): any {
+    return this.to.options;
+  }
 }
