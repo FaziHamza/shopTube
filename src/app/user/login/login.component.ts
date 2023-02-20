4987ae742e09391869d93846fcadb09dd281fe12
Faizan Hamza <Faizan1712818@gmail.com>
2023-02-20 20:57:43 +0500

    LOGIN

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +12,18 @@ export class LoginComponent implements OnInit {
   validateForm!: UntypedFormGroup;
 
   submitForm(): void {
+    debugger
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
-    } else {
+      this.employeeService.login(this.validateForm.value.email
+        , this.validateForm.value.password
+        ).subscribe((res=>{
+        if(res.length>0){
+          this.router.navigate(['/']);
+        }
+      }))
+    } 
+    else {
       Object.values(this.validateForm.controls).forEach(control => {
         if (control.invalid) {
           control.markAsDirty();
@@ -22,12 +33,13 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  constructor(private fb: UntypedFormBuilder) {}
+  constructor(private fb: UntypedFormBuilder , public employeeService: EmployeeService , private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
-      email: [null, [Validators.required]],
-      password: [null, [Validators.required]],
+      email: ['admin@co.co', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required]],
       remember: [true]
     });
   }
