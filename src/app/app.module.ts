f85900e4f9de5eac02111732cde91f036f5da24f
arfanali278 <arfan7452@gmail.com>
2023-02-21 18:59:39 +0500

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,8 +50,6 @@ import { InvoiceTemplateComponent } from './components/invoice-template/invoice-
 import { AudioComponent } from './components/audio/audio.component';
 import { RateComponent } from './components/rate/rate.component';
 import { TableComponent } from './components/table/table.component';
-import { FormlyFieldNgSearchComponent } from './wrappers/formly-field-ng-search.component';
-import { FormlyFieldNgSelectComponent } from './wrappers/formly-field-ng-select.component';
 import { AffixComponent } from './components/affix/affix.component';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { TransferComponent } from './components/transfer/transfer.component';
@@ -104,8 +102,7 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesign
     LoginComponent,
     RegisterComponent,
     RateComponent,
-    TableComponent
-    RegisterComponent,
+    TableComponent,
     AffixComponent,
     BreadCrumbComponent,
     TransferComponent,
