04383998cfc6bea7ef0e9378f5fa6cf3c169597d
arfanali278 <arfan7452@gmail.com>
2023-03-25 17:36:42 +0500

    props

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -5599,46 +5599,46 @@ export class BuilderComponent implements OnInit {
             if (event.tableDta) {
               props['options'] = event.tableDta;
             }
-            templateOptions['required'] = event.form.required;
-            templateOptions['maxLength'] = event.form.maxLength;
-            templateOptions['minLength'] = event.form.minLength;
-            templateOptions['disabled'] = event.form.disabled;
-            templateOptions['tooltip'] = event.form.tooltip;
-            templateOptions['className'] = event.form.className;
-            templateOptions['titleIcon'] = event.form.titleIcon;
-            if(templateOptions.config.wrapper == 'floating_filled' || templateOptions.config.wrapper == 'floating_filled' || templateOptions.config.wrapper == 'floating_standard'){
-              templateOptions.config['addonRight'] = '';
-            templateOptions.config['addonLeft'] = '';
-            templateOptions.config['prefixicon'] = '';
-            templateOptions.config['suffixicon'] = '';
+            props['required'] = event.form.required;
+            props['maxLength'] = event.form.maxLength;
+            props['minLength'] = event.form.minLength;
+            props['disabled'] = event.form.disabled;
+            props['tooltip'] = event.form.tooltip;
+            props['className'] = event.form.className;
+            props['titleIcon'] = event.form.titleIcon;
+            if(props.config.wrapper == 'floating_filled' || props.config.wrapper == 'floating_filled' || props.config.wrapper == 'floating_standard'){
+              props.config['addonRight'] = '';
+            props.config['addonLeft'] = '';
+            props.config['prefixicon'] = '';
+            props.config['suffixicon'] = '';
             }else{
-              templateOptions.config['addonRight'] = event.form.addonRight;
-              templateOptions.config['addonLeft'] = event.form.addonLeft;
-              templateOptions.config['prefixicon'] = event.form.prefixicon;
-              templateOptions.config['suffixicon'] = event.form.suffixicon;
+              props.config['addonRight'] = event.form.addonRight;
+              props.config['addonLeft'] = event.form.addonLeft;
+              props.config['prefixicon'] = event.form.prefixicon;
+              props.config['suffixicon'] = event.form.suffixicon;
             }
-            templateOptions.config['border'] = event.form.border;
-            templateOptions.config['optionWidth'] = event.form.optionWidth;
-            templateOptions.config['step'] = event.form.step;
-            templateOptions.config['format'] = event.form.format;
-            templateOptions.config['allowClear'] = event.form.allowClear;
-            templateOptions.config['serveSearch'] = event.form.serveSearch;
-            templateOptions.config['showArrow'] = event.form.showArrow;
-            templateOptions.config['showSearch'] = event.form.showSearch;
-            templateOptions.config['clearIcon'] = event.form.clearIcon;
-            templateOptions.config['loading'] = event.form.loading;
-            templateOptions.config['optionHieght'] = event.form.optionHieght;
-            templateOptions.config['optionHoverSize'] = event.form.optionHoverSize;
-            templateOptions.config['optionDisabled'] = event.form.optionDisabled;
-            templateOptions.config['optionHide'] = event.form.optionHide;
-            templateOptions.config['firstBtnText'] = event.form.firstBtnText;
-            templateOptions.config['secondBtnText'] = event.form.secondBtnText;
-            templateOptions.config['minuteStep'] = event.form.minuteStep;
-            templateOptions.config['secondStep'] = event.form.secondStep;
-            templateOptions.config['hoursStep'] = event.form.hoursStep;
-            templateOptions.config['use12Hours'] = event.form.use12Hours;
-            templateOptions.config['icon'] = event.form.icon;
-            templateOptions['readonly'] = event.form.readonly;
+            props.config['border'] = event.form.border;
+            props.config['optionWidth'] = event.form.optionWidth;
+            props.config['step'] = event.form.step;
+            props.config['format'] = event.form.format;
+            props.config['allowClear'] = event.form.allowClear;
+            props.config['serveSearch'] = event.form.serveSearch;
+            props.config['showArrow'] = event.form.showArrow;
+            props.config['showSearch'] = event.form.showSearch;
+            props.config['clearIcon'] = event.form.clearIcon;
+            props.config['loading'] = event.form.loading;
+            props.config['optionHieght'] = event.form.optionHieght;
+            props.config['optionHoverSize'] = event.form.optionHoverSize;
+            props.config['optionDisabled'] = event.form.optionDisabled;
+            props.config['optionHide'] = event.form.optionHide;
+            props.config['firstBtnText'] = event.form.firstBtnText;
+            props.config['secondBtnText'] = event.form.secondBtnText;
+            props.config['minuteStep'] = event.form.minuteStep;
+            props.config['secondStep'] = event.form.secondStep;
+            props.config['hoursStep'] = event.form.hoursStep;
+            props.config['use12Hours'] = event.form.use12Hours;
+            props.config['icon'] = event.form.icon;
+            props['readonly'] = event.form.readonly;
             if (event.tableDta) {
               props['options'] = event.tableDta;
             } else {
@@ -7270,41 +7270,41 @@ export class BuilderComponent implements OnInit {
           fieldGroup[0].props.labelPosition = "text-left";
         }
         if (formValues.status) {
-          fieldGroup[0].templateOptions.config.status = formValues.status;
+          fieldGroup[0].props.config.status = formValues.status;
         }
         if (formValues.size) {
-          fieldGroup[0].templateOptions.config.size = formValues.size;
+          fieldGroup[0].props.config.size = formValues.size;
         }
         if (formValues.wrappers == 'floating_filled') {
           fieldGroup[0].wrappers[0] = [formValues.wrappers][0];
-          fieldGroup[0].templateOptions.config['floatFieldClass'] = 'block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer';
-          fieldGroup[0].templateOptions.config['floatLabelClass'] = 'absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4';
-          fieldGroup[0].templateOptions.config['wrapper'] = [formValues.wrappers][0];
-          fieldGroup[0].templateOptions.placeholder = " ";
-          if (fieldGroup[0].templateOptions.config.size == 'small' || fieldGroup[0].templateOptions.config.size == 'large') {
-            fieldGroup[0].templateOptions.config.size = 'default';
+          fieldGroup[0].props.config['floatFieldClass'] = 'block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer';
+          fieldGroup[0].props.config['floatLabelClass'] = 'absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4';
+          fieldGroup[0].props.config['wrapper'] = [formValues.wrappers][0];
+          fieldGroup[0].props.placeholder = " ";
+          if (fieldGroup[0].props.config.size == 'small' || fieldGroup[0].props.config.size == 'large') {
+            fieldGroup[0].props.config.size = 'default';
             this.selectedNode.size = 'default';
           }
         }
         else if (formValues.wrappers == 'floating_outlined') {
           fieldGroup[0].wrappers[0] = [formValues.wrappers][0];
-          fieldGroup[0].templateOptions.config['floatFieldClass'] = 'block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer';
-          fieldGroup[0].templateOptions.config['floatLabelClass'] = 'absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1';
-          fieldGroup[0].templateOptions.config['wrapper'] = [formValues.wrappers][0]
-          fieldGroup[0].templateOptions.placeholder = " ";
-          if (fieldGroup[0].templateOptions.config.size == 'small' || fieldGroup[0].templateOptions.config.size == 'large') {
-            fieldGroup[0].templateOptions.config.size = 'default';
+          fieldGroup[0].props.config['floatFieldClass'] = 'block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer';
+          fieldGroup[0].props.config['floatLabelClass'] = 'absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1';
+          fieldGroup[0].props.config['wrapper'] = [formValues.wrappers][0]
+          fieldGroup[0].props.placeholder = " ";
+          if (fieldGroup[0].props.config.size == 'small' || fieldGroup[0].props.config.size == 'large') {
+            fieldGroup[0].props.config.size = 'default';
             this.selectedNode.size = 'default';
           }
         }
         else if (formValues.wrappers == 'floating_standard') {
           fieldGroup[0].wrappers[0] = [formValues.wrappers][0];
-          fieldGroup[0].templateOptions.config['floatFieldClass'] = 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer';
-          fieldGroup[0].templateOptions.config['floatLabelClass'] = 'absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6';
-          fieldGroup[0].templateOptions.config['wrapper'] = [formValues.wrappers][0];
-          fieldGroup[0].templateOptions.placeholder = " ";
-          if (fieldGroup[0].templateOptions.config.size == 'small' || fieldGroup[0].templateOptions.config.size == 'large') {
-            fieldGroup[0].templateOptions.config.size = 'default';
+          fieldGroup[0].props.config['floatFieldClass'] = 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer';
+          fieldGroup[0].props.config['floatLabelClass'] = 'absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6';
+          fieldGroup[0].props.config['wrapper'] = [formValues.wrappers][0];
+          fieldGroup[0].props.placeholder = " ";
+          if (fieldGroup[0].props.config.size == 'small' || fieldGroup[0].props.config.size == 'large') {
+            fieldGroup[0].props.config.size = 'default';
             this.selectedNode.size = 'default';
           }
         }
