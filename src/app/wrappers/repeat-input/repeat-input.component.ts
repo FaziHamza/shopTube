70d4f0f501a79910ff873e6dc64b801317d2d58a
arfanali278 <arfan7452@gmail.com>
2023-04-11 13:15:26 +0500

    repeat input

--- src/app/wrappers/repeat-input/repeat-input.component.ts
+++ src/app/wrappers/repeat-input/repeat-input.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {  UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { FieldType, FieldTypeConfig } from '@ngx-formly/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'st-repeat-input',
@@ -7,25 +9,34 @@ import { FieldType, FieldTypeConfig } from '@ngx-formly/core';
   styleUrls: ['./repeat-input.component.scss']
 })
 export class RepeatInputComponent extends FieldType<FieldTypeConfig> {
-  listOfControl :any = [];
-  ngOnInit(): void {
-    this.listOfControl.push(this.to);
+  subscription: Subscription;
+  listOfControl: Array<{ id: number; controlInstance: string }> = [];
+  constructor(private fb: UntypedFormBuilder) {
+    super();
   }
-  
+  validateForm!: UntypedFormGroup;
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({});
+    this.addField();
+  }
+
   addField(e?: MouseEvent): void {
-    debugger
     if (e) {
       e.preventDefault();
     }
     const id = this.listOfControl.length > 0 ? this.listOfControl[this.listOfControl.length - 1].id + 1 : 0;
-
     const control = {
       id,
-      controlInstance: id
+      controlInstance: id.toString()
     };
     const index = this.listOfControl.push(control);
-    console.log(this.listOfControl[this.listOfControl.length - 1]);
-   
+    this.validateForm.addControl(
+      this.listOfControl[index - 1].controlInstance,
+      new UntypedFormControl(null, Validators.required)
+    );
+    this.subscription = this.validateForm.valueChanges.subscribe(value => {
+     this.formControl.patchValue(this.validateForm.value);
+    });
   }
 
   removeField(i: { id: number; controlInstance: string }, e: MouseEvent): void {
@@ -33,8 +44,11 @@ export class RepeatInputComponent extends FieldType<FieldTypeConfig> {
     if (this.listOfControl.length > 1) {
       const index = this.listOfControl.indexOf(i);
       this.listOfControl.splice(index, 1);
-      console.log(this.listOfControl);
+      this.validateForm.removeControl(index.toString());
+      this.formControl.patchValue(this.validateForm.value);
     }
   }
-
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
