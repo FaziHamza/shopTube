0be83b99c1888e30db07f3bb45ff8d6ab4a3994b
Faizan Hamza <Faizan1712818@gmail.com>
2023-06-04 18:08:02 +0500

    file manager work

--- src/app/components/file-manager/file-manager.component.ts
+++ src/app/components/file-manager/file-manager.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-manager.component.scss']
 })
 export class FileManagerComponent implements OnInit {
+  breadCrumbItems!: Array<{}>;
+  recent!: any;
+  storageChart!: any;
+
+  isVisible: Boolean = true;
 
   constructor() { }
+
+  toggle() {
+    this.isVisible = !this.isVisible;
+  }
+
+  remove() {
+    document.getElementById('alertremove')!.classList.remove('show');
+  }
 
   ngOnInit(): void {
   }
