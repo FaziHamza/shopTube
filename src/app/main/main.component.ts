b60921c9ec6ea62531e48d76096f8d5e6605a452
arfanali278 <arfan7452@gmail.com>
2023-08-13 18:41:47 +0500

    main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -256,45 +256,6 @@ export class MainComponent implements OnInit {
       }
     }
   }
-  saveData(data: any) {
-    if (data.isSubmit) {
-      let oneModelData = this.convertModel(this.form.value);
-      // this.mainData.children
-      // this.mainData.children.forEach((element:any) => {
-      //   if(element.type == "gridList"){
-      //     let tableData = this.findObjectByType(element,"gridList");
-      //     tableData.tableData = [];
-      //     tableData.tableHeaders = [];
-      //     tableData?.tableData?.push(this.form.value);
-      //   }
-      // });
-      // let tableData = this.findObjectByType(element,"gridList");
-      let tableData = this.mainData.children.filter((a: any) => a.type == "gridList");
-      if (tableData.length > 0) {
-        tableData[0]['api'] = data.dataTable;
-        let saveForm = JSON.parse(JSON.stringify(oneModelData));
-        // saveForm["id"] = '';
-        // this.form.value["id"] = tableData[0]['tableKey'].length
-        const firstObjectKeys = Object.keys(saveForm);
-        let obj = firstObjectKeys.map(key => ({ name: key }));
-        // obj.unshift({name: 'id'});
-        if (JSON.stringify(tableData[0]['tableKey']) != JSON.stringify(obj)) {
-          tableData[0].tableData = [];
-          tableData[0]['tableKey'] = obj;
-          tableData[0].tableHeaders = tableData[0]['tableKey'];
-          saveForm.id = tableData[0].tableData.length + 1
-          tableData[0].tableData?.push(saveForm);
-        } else {
-          tableData[0]['tableKey'] = obj;
-          tableData[0].tableHeaders = tableData[0]['tableKey'];
-          saveForm.id = tableData[0].tableData.length + 1;
-          tableData[0].tableData?.push(saveForm);
-        }
-
-      }
-      this.saveData1(data);
-    }
-  }
   setInternalValuesEmpty = (obj: any) => {
     for (const key in obj) {
       if (typeof obj[key] === 'object' && obj[key] !== null) {
@@ -304,143 +265,7 @@ export class MainComponent implements OnInit {
       }
     }
   };
-  saveData1(data: any) {
-    let oneModelData = this.convertModel(this.form.value);
 
-    // const objModel: any = this.form.value;
-    // var nestedObject = {};
-    // for (var key in objModel) {
-    //   Object.assign(nestedObject, objModel[key]);
-    // }
-
-    // let nestedObject: any = null;
-    // Object.keys(objModel).forEach(key => {
-    //   const value = objModel[key];
-    //   if (typeof value === "object" && value !== null) {
-    //     nestedObject = value;
-    //   }
-    // });
-    const empData: any = {
-      screenId: this.screenName,
-      modalData: oneModelData
-    };
-
-    console.log(empData);
-    const tableNames = new Set();
-
-    for (const key in empData.modalData) {
-      const tableName = key.split('.')[0];
-      tableNames.add(tableName);
-    }
-
-    const Arraytables = Array.from(tableNames)
-    const remainingTables = Arraytables.slice(1);
-    let id;
-    for (const key in empData.modalData) {
-      if (empData.modalData.hasOwnProperty(key) &&
-        key.endsWith('.id') &&
-        empData.modalData[key] !== "") {
-        id = key;
-      }
-    }
-
-    if (id == undefined) {
-      let relationIds: any = remainingTables.map(table => `${Arraytables[0]}_id`);
-      relationIds = relationIds.toString();
-      const tables = (Array.from(tableNames)).toString();
-      console.log(tables);
-      this.employeeService.saveSQLDatabaseTable('knex-query', empData)
-        .pipe(
-          catchError((error) => {
-            console.error(error);
-            if (error.status === 400 && error.error && error.error.message) {
-              this.toastr.error(error.error.message, { nzDuration: 3000 });
-            } else {
-              this.toastr.error('An error occurred', { nzDuration: 3000 });
-            }
-            return throwError(error); // Rethrow the error
-          })
-        )
-        .subscribe((res) => {
-          // Handle success response or further logic
-          this.toastr.success('Save Successfully', { nzDuration: 3000 });
-          this.setInternalValuesEmpty(this.form.value);
-          this.getFromQuery();
-        });
-    } else {
-      const dynamicPropertyName = Object.keys(this.form.value)[0]; // Assuming the dynamic property name is the first property in this.form.value
-      if (this.form.get(dynamicPropertyName)) {
-        this.form.get(dynamicPropertyName);
-        const model = {
-          screenId: this.screenName,
-          postType: 'put',
-          modalData: this.form.value[dynamicPropertyName]
-        };
-
-        this.employeeService.saveSQLDatabaseTable('knex-query/executeQuery', model).subscribe({
-          next: (res) => {
-            this.toastr.success("Update Successfully", { nzDuration: 3000 });
-            this.getFromQuery();
-          },
-          error: (err) => {
-            console.error(err);
-            this.toastr.error("An error occurred", { nzDuration: 3000 });
-          }
-        });
-      }
-    }
-
-    // if (data.dataTable) {
-    //   this.requestSubscription = this.builderService.genericApisPost(data.dataTable, this.form.value).subscribe({
-    //     next: (res) => {
-    //       this.toastr.success("Data saved!", { nzDuration: 3000 });
-    //     },
-    //     error: (err) => {
-    //       console.error(err); // Log the error to the console
-    //       this.toastr.error("An error occurred", { nzDuration: 3000 }); // Show an error message to the user
-    //     }
-    //   });
-    // } else {
-    //   this.toastr.error("Data table required", { nzDuration: 3000 }); // Show an error message to the user
-    // }
-  }
-  getFromQuery() {
-    let tableData = this.mainData.children.filter((a: any) => a.type == "gridList");
-    this.employeeService.getSQLDatabaseTable(`knex-query/${this.screenName}`).subscribe({
-      next: (res) => {
-
-        if (tableData.length > 0) {
-          // tableData[0]['api'] = data.dataTable;
-          let saveForm = JSON.parse(JSON.stringify(res[0]));
-          // saveForm["id"] = '';
-          // this.form.value["id"] = tableData[0]['tableKey'].length
-          const firstObjectKeys = Object.keys(saveForm);
-          let obj = firstObjectKeys.map(key => ({ name: key }));
-          // obj.unshift({name: 'id'});
-          if (JSON.stringify(tableData[0]['tableKey']) != JSON.stringify(obj)) {
-            tableData[0].tableData = [];
-            tableData[0]['tableKey'] = obj;
-            tableData[0].tableHeaders = tableData[0]['tableKey'];
-            saveForm.id = tableData[0].tableData.length + 1
-            res.forEach((element: any) => {
-              element.id = (element.id).toString();
-              tableData[0].tableData?.push(element);
-            });
-          } else {
-            tableData[0]['tableKey'] = obj;
-            tableData[0].tableHeaders = tableData[0]['tableKey'];
-            tableData[0].tableData = [];
-            saveForm.id = tableData[0].tableData.length + 1;
-            res.forEach((element: any) => {
-              element.id = (element.id).toString();
-              tableData[0].tableData?.push(element);
-            });
-            // tableData[0].tableData?.push(saveForm);
-          }
-        }
-      }
-    });
-  }
   findObjectByType(data: any, key: any) {
     if (data.type === key) {
       return data;
@@ -488,5 +313,5 @@ export class MainComponent implements OnInit {
       }
     });
   }
-  
+
 }
