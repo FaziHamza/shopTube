9aa3568f5e3b54a85ba4e9b925c888e888250eec
Faizan Hamza <Faizan1712818@gmail.com>
2023-09-06 19:58:36 +0500

    builder fixing

--- src/app/components/drawer/drawer.component.ts
+++ src/app/components/drawer/drawer.component.ts
@@ -15,9 +15,6 @@ export class DrawerComponent implements OnInit {
   @Input() screenName: any;
   @Input() drawerData: any;
   @Input() showModal = true;
-  bgColor : any;
-  borderColor : any;
-  hoverTextColor : any;
   nodes: TreeNode[];
   loader: boolean = false
   requestSubscription: Subscription;
