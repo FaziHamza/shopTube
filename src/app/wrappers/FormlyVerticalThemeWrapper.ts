baa9288208fd762c5f57192cef01d7fe9062cc01
Muhammad Tayyab Baig <116713172+MTAYABAIG@users.noreply.github.com>
2023-09-25 19:25:56 +0500

    validation

--- src/app/wrappers/FormlyVerticalThemeWrapper.ts
+++ src/app/wrappers/FormlyVerticalThemeWrapper.ts
@@ -6,9 +6,10 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'formly-vertical-theme-wrapper',
   template: `
+  {{ hasError | json}}
   <span *ngIf="to['additionalProperties']?.tooltip && to['additionalProperties']?.tooltipPosition == 'top' && !to['additionalProperties']?.tooltipWithoutIcon || false" nz-tooltip [nzTooltipTitle]="to['additionalProperties']?.tooltip">
   <span nz-icon [nzType]="to['additionalProperties']['tooltipIcon'] ?  to['additionalProperties']['tooltipIcon'] : 'question-circle'" [class]="to['additionalProperties']['toolTipClass']" nzTheme="outline"></span></span>
-  <div class=" flex flex-wrap" [dir]="to['additionalProperties']?.formatAlignment || 'ltr'">
+  <div [ngClass]="{'errorClass': hasError }" class="flex flex-wrap" [dir]="to['additionalProperties']?.formatAlignment || 'ltr'">
     <label [attr.for]="id" class="w-1/3 py-2 col-form-label relative column-form-label {{to['additionalProperties']?.labelPosition}}" *ngIf="to.label">
     <span  *ngIf="to['additionalProperties']?.tooltip && to['additionalProperties']['tooltipPosition'] == 'left' && !to['additionalProperties']?.tooltipWithoutIcon || false" nz-tooltip [nzTooltipTitle]="to['additionalProperties']?.tooltip">
       <span nz-icon [nzType]="to['additionalProperties']['tooltipIcon'] ?  to['additionalProperties']['tooltipIcon'] : 'question-circle'" [class]="to['additionalProperties']['toolTipClass']" nzTheme="outline"></span>
@@ -20,7 +21,7 @@ import { Subscription } from 'rxjs';
                 [type]="'pages'"></st-task-report>
       <span [class]="to['additionalProperties']?.labelClassName">{{to.label | translate}}</span>
       <span *ngIf="to.required" class="text-red-600">*</span>
-      <span *ngIf="to['additionalProperties']?.tooltip && (to['additionalProperties']['tooltipPosition'] == 'right' || to['additionalProperties']?.tooltipPosition == undefined) && !to['additionalProperties']?.tooltipWithoutIcon || false" nz-tooltip [nzTooltipTitle]="to['additionalProperties']?.tooltip">
+      <span  *ngIf="to['additionalProperties']?.tooltip && (to['additionalProperties']['tooltipPosition'] == 'right' || to['additionalProperties']?.tooltipPosition == undefined) && !to['additionalProperties']?.tooltipWithoutIcon || false" nz-tooltip [nzTooltipTitle]="to['additionalProperties']?.tooltip">
         <span nz-icon [nzType]="to['additionalProperties']['tooltipIcon'] ?  to['additionalProperties']['tooltipIcon'] : 'question-circle'" [class]="to['additionalProperties']['toolTipClass']" nzTheme="outline"></span>
       </span>
     </label>
@@ -28,7 +29,7 @@ import { Subscription } from 'rxjs';
       <ng-template #fieldComponent></ng-template>
     </div>
     <div *ngIf="hasError && to.label" class="w-1/3 {{to['additionalProperties']?.labelPosition}}"></div>
-    <div *ngIf="hasError" class="w-2/3 text-red-500 text-sm block" style="padding: 0px">
+    <div *ngIf="hasError" class="w-2/3 text-red-500 font-semibold text-sm block p-1.5 border border-gray-300 bg-white">
       <span>{{to['additionalProperties']?.requiredMessage}}</span>
       <!-- <formly-validation-message [field]="field"></formly-validation-message> -->
     </div>
