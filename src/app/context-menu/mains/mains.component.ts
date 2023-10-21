5d72b728e1a7556e84f6ad50de846c2a077639c6
arfanali278 <arfan7452@gmail.com>
2023-10-21 20:35:11 +0500

    notify

--- src/app/context-menu/mains/mains.component.ts
+++ src/app/context-menu/mains/mains.component.ts
@@ -38,9 +38,7 @@ export class MainsComponent implements OnInit {
     ];
     this.nzImageService.preview(images, { nzZoom: data.zoom, nzRotate: data.rotate, nzKeyboard: data.keyboardKey, nzZIndex: data.zIndex });
   }
-  saveData(data: any) {
-    this.notify.emit(data);
-  }
+  
 
   handleIndexChange(e: number): void {
     console.log(e);
