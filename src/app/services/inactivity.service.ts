e379a0eaf404296cc677dac5ff66c6fc77d083e7
faizan <faizan1712818@gmail.com>
2023-11-11 17:16:38 +0500

    Fixed and cehck

--- src/app/services/inactivity.service.ts
+++ src/app/services/inactivity.service.ts
@@ -7,51 +7,62 @@ import { switchMap, tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class InactivityService implements OnDestroy {
-  private inactivityTimeout = 10 * 60 * 1000; // 10 minutes in milliseconds
+  private inactivityTimeout = 1 * 60 * 1000; // 10 minutes in milliseconds
   private localStorageKey = 'lastActivityTime';
   private isLoggedInKey = 'isLoggedIn';
   private userActivitySubscription: Subscription;
 
   constructor(private router: Router) {
-    // this.startWatchingForInactivity();
-    // this.startWatchingStorageEvents();
+    this.startWatchingForInactivity();
+    this.startWatchingStorageEvents();
   }
 
-  // private startWatchingForInactivity() {
-  //   this.userActivitySubscription = timer(0, 1000).pipe(
-  //     switchMap(() => {
-  //       if (localStorage.getItem(this.isLoggedInKey) !== 'true') {
-  //         return timer(0); // Not logged in, navigate to login
-  //       }
-  //       const lastActivity = parseInt(localStorage.getItem(this.localStorageKey) || '0', 10);
-  //       const timePassed = Date.now() - lastActivity;
-  //       return timePassed >= this.inactivityTimeout ? timer(0) : timer(this.inactivityTimeout - timePassed);
-  //     }),
-  //     tap(() => this.logout())
-  //   ).subscribe();
-  // }
+  private startWatchingForInactivity() {
+    this.userActivitySubscription = timer(0, 1000).pipe(
+      switchMap(() => {
+        const isLoggedIn = localStorage.getItem(this.isLoggedInKey) === 'true';
+        console.log('Checking activity. Is Logged In:', isLoggedIn);
+
+        if (!isLoggedIn) {
+          return timer(0); // Not logged in, navigate to login
+        }
+
+        const lastActivity = parseInt(localStorage.getItem(this.localStorageKey) || '0', 10);
+        const timePassed = Date.now() - lastActivity;
+        const timeLeft = this.inactivityTimeout - timePassed;
+        console.log('Time passed:', timePassed, 'Time left:', timeLeft);
+
+        return timeLeft <= 0 ? timer(0) : timer(timeLeft);
+      }),
+      tap(() => {
+        console.log('Executing logout due to inactivity.');
+        this.logout();
+      })
+    ).subscribe();
+  }
 
   private startWatchingStorageEvents() {
     fromEvent<StorageEvent>(window, 'storage').pipe(
       tap((event: StorageEvent | null) => {
         if (event && event.key === this.localStorageKey && localStorage.getItem(this.isLoggedInKey) !== 'true') {
-          // this.logout();
+          console.log('Detected logout from another tab.');
+          this.logout();
         }
       })
     ).subscribe();
   }
 
   updateUserActivity() {
-    // If the user is active in any tab, update the activity timestamp in localStorage
+    console.log('User activity detected.');
     localStorage.setItem(this.localStorageKey, Date.now().toString());
   }
 
-  // logout() {
-  //   console.log('User has been logged out due to inactivity or user action.');
-  //   localStorage.removeItem(this.isLoggedInKey); // Clear the logged-in flag
-  //   localStorage.clear();
-  //   this.router.navigate(['/login']);
-  // }
+  logout() {
+    console.log('Logging out user.');
+    localStorage.removeItem(this.isLoggedInKey); // Clear the logged-in flag
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 
   ngOnDestroy() {
     if (this.userActivitySubscription) {
