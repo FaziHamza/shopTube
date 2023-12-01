a99622e79aed78ebbcfe2a1fcb541a9fcf443e08
hasnainatique <hasnainatique786@gmail.com>
2023-12-01 12:33:23 +0500

    support chat validation fixed

--- src/app/admin/support-chat/support-chat.component.ts
+++ src/app/admin/support-chat/support-chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { Subscription } from 'rxjs';
@@ -26,7 +26,8 @@ export class SupportChatComponent {
   editDeleteId: any;
   editId: any;
   constructor(public dataSharedService: DataSharedService, private toastr: NzMessageService,
-    private applicationServices: ApplicationService, private employeeService: EmployeeService, private modal: NzModalService) {
+    private applicationServices: ApplicationService, private employeeService: EmployeeService, private modal: NzModalService,
+    private cdr: ChangeDetectorRef) {
     this.processData = this.processData.bind(this);
   }
 
@@ -46,11 +47,14 @@ export class SupportChatComponent {
   }
   saveChat() {
     debugger
+    if (this.comment == '' || this.comment == undefined || this.comment == null) {
+      return;
+    }
     const checkPermission = this.dataSharedService.getUserPolicyMenuList.find(a => a.screenId === this.dataSharedService.currentMenuLink);
-    // if (!checkPermission?.create && this.dataSharedService?.currentMenuLink !== '/ourbuilder') {
-    //   this.toastr.warning("You do not have permission", { nzDuration: 3000 });
-    //   return;
-    // }
+    if (!checkPermission?.create && this.dataSharedService?.currentMenuLink !== '/ourbuilder') {
+      this.toastr.warning("You do not have permission", { nzDuration: 3000 });
+      return;
+    }
     const postEvent = this.data.appConfigurableEvent.find((item: any) => item.rule.includes('post_'));
     const putEvent = this.data.appConfigurableEvent.find((item: any) => item.rule.includes('put_'));
     if (!postEvent && !putEvent) {
@@ -72,7 +76,6 @@ export class SupportChatComponent {
     let actionID = this.editId ? putEvent?._id : postEvent._id;
     if (this.editId) {
       model['modalData']['ticketcomments.id'] = this.editId;
-      model['modalData']['ticketcomments.screenid'] = this.screenName;
     }
     this.saveLoader = true;
     if (actionID) {
@@ -83,24 +86,22 @@ export class SupportChatComponent {
             this.toastr.error(res[0]?.error, { nzDuration: 3000 });
             return;
           }
-          if (model.postType === 'put') {
-            if (!res?.isSuccess) {
-              this.toastr.error(res.message, { nzDuration: 3000 });
-              return;
-            }
+          if (model.postType === 'put' && !res?.isSuccess) {
+            this.toastr.error(res.message, { nzDuration: 3000 });
+            return;
           }
-          if (model.postType === 'put') {
-            this.chatData = this.chatData.map((obj: any) => (obj.id === this.editId ? { ...obj, comment: this.comment } : obj));
-          } else {
-            let obj: any = {};
-            let responseObject = res[0];
-            for (let key in responseObject) {
-              obj[key.split('.')[1]] = responseObject[key];
-            }
-            this.chatData.push(obj);
-          }
-          this.comment = '';
-          this.editId = '';
+          // if (model.postType === 'put') {
+          //   this.chatData = this.chatData.map((obj: any) => (obj.id === this.editId ? { ...obj, comment: this.comment } : obj));
+          // } else {
+          //   let obj: any = {};
+          //   let responseObject = res[0];
+          //   for (let key in responseObject) {
+          //     obj[key.split('.')[1]] = responseObject[key];
+          //   }
+          //   this.chatData.push(obj);
+          // }
+          this.getChats();
+          this.resetValues();
           const successMessage = (model.postType === 'post') ? 'Save Successfully' : 'Update Successfully';
           this.toastr.success(successMessage, { nzDuration: 3000 });
 
@@ -122,38 +123,38 @@ export class SupportChatComponent {
     this.editDeleteId = id;
   }
   delete(data: any) {
-    // const checkPermission = this.dataSharedService.getUserPolicyMenuList.find(a => a.screenId == this.dataSharedService.currentMenuLink);
-    // if (!checkPermission?.delete && this.dataSharedService.currentMenuLink != '/ourbuilder') {
-    //   alert("You did not have permission");
-    //   return;
-    // }
+    const checkPermission = this.dataSharedService.getUserPolicyMenuList.find(a => a.screenId == this.dataSharedService.currentMenuLink);
+    if (!checkPermission?.delete && this.dataSharedService.currentMenuLink != '/ourbuilder') {
+      alert("You did not have permission");
+      return;
+    }
     const model = {
       screenId: this.screenName,
       postType: 'delete',
       modalData: data
     };
     if (this.screenName != undefined) {
-      if (this.data?.appConfigurableEvent && this.data?.appConfigurableEvent?.length > 0) {
-        const findClickApi = this.data.appConfigurableEvent.find((item: any) => item.rule.includes('delete'));
-        if (findClickApi) {
-          this.saveLoader = true;
-          this.requestSubscription = this.employeeService.saveSQLDatabaseTable(`knex-query/executeDelete-rules/${findClickApi._id}`, model).subscribe({
-            next: (res) => {
-              this.saveLoader = false;
-              if (res.isSuccess) {
-                this.toastr.success("Delete Successfully", { nzDuration: 3000 });
-                this.chatData = this.chatData.filter((a: any) => a.id != data.id)
-              } else {
-                this.toastr.warning(res.message || "Data is not deleted", { nzDuration: 3000 });
-              }
-            },
-            error: (err) => {
-              this.saveLoader = false;
-              this.toastr.error(`An error occurred ${err}`, { nzDuration: 3000 });
-            }
-          });
-        }
+      const findClickApi = this.data.appConfigurableEvent.find((item: any) => item.rule.includes('delete'));
+      if (!findClickApi) {
+        this.toastr.warning("Action not found", { nzDuration: 3000 });
       }
+      this.saveLoader = true;
+      this.requestSubscription = this.employeeService.saveSQLDatabaseTable(`knex-query/executeDelete-rules/${findClickApi._id}`, model).subscribe({
+        next: (res) => {
+          this.saveLoader = false;
+          this.resetValues();
+          if (res.isSuccess) {
+            this.toastr.success("Delete Successfully", { nzDuration: 3000 });
+            this.chatData = this.chatData.filter((a: any) => a.id != data.id)
+          } else {
+            this.toastr.warning(res.message || "Data is not deleted", { nzDuration: 3000 });
+          }
+        },
+        error: (err) => {
+          this.saveLoader = false;
+          this.toastr.error(`An error occurred ${err}`, { nzDuration: 3000 });
+        }
+      });
     }
   }
   showDeleteConfirm(rowData: any): void {
@@ -171,27 +172,33 @@ export class SupportChatComponent {
   edit(data: any) {
     this.editId = data.id;
     this.comment = data.comment;
+    this.cdr.detectChanges();
   }
-  // getChats() {
-  //   debugger
-  //   this.saveLoader = true;
-  //   const getEvent = this.data.appConfigurableEvent.find((item: any) => item.rule.includes('get_'));
-  //   if (!getEvent) {
-  //     return;
-  //   }
-  //   this.applicationServices.callApi('knex-query/getexecute-rules/' + getEvent._id, 'get', '', '', '').subscribe({
-  //     next: (res) => {
-  //       this.saveLoader = false; // Ensure to set the loader to false in case of error
-  //       if (res.isSuccess) {
-  //         this.chatData = res.data;
-  //       }
-  //     },
-  //     error: (err) => {
-  //       // Handle the error
-  //       this.toastr.error("An error occurred", { nzDuration: 3000 });
-  //       console.error(err);
-  //       this.saveLoader = false; // Ensure to set the loader to false in case of error
-  //     },
-  //   });
-  // }
+  resetValues() {
+    this.comment = '';
+    this.editId = '';
+    this.editDeleteId = '';
+  }
+  getChats() {
+    debugger
+    this.saveLoader = true;
+    if (this.data.eventActionconfig) {
+      if (this.data.eventActionconfig.action) {
+        this.applicationServices.callApi('knex-query/getexecute-rules/' + this.data.eventActionconfig._id, 'get', '', '', '').subscribe({
+          next: (res) => {
+            this.saveLoader = false; // Ensure to set the loader to false in case of error
+            if (res.isSuccess) {
+              this.chatData = res.data;
+            }
+          },
+          error: (err) => {
+            // Handle the error
+            this.toastr.error("An error occurred", { nzDuration: 3000 });
+            console.error(err);
+            this.saveLoader = false; // Ensure to set the loader to false in case of error
+          },
+        });
+      }
+    }
+  }
 }
